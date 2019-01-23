07845fc79a533328a22fb39da40156bc78193268
Mildred34 <alexis-h-34@hotmail.fr>
2019-01-23 11:43:09 +0100

    Ajout d'un test cmath

---
+++ cmath/cmathtest.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	double a(4.3), b(5.2);
+	cout << pow(a,b) << endl;
+	cout << sqrt(a) << endl;
+	cout << cos(b) << endl;
+	return 0;
+}
