58ffb385dfef871aade072b0885782b28082d289
Mildred34 <alexis-h-34@hotmail.fr>
2019-01-27 18:37:45 +0100

    Ajout exemple sur les itérateurs des list

---
+++ List_E/List_T.cpp
@@ -0,0 +1,18 @@
+#include <list>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    list<int> liste;       //Une liste d'entiers
+    liste.push_back(5);    //On ajoute un entier dans la liste
+    liste.push_back(8);    //Et un deuxième
+    liste.push_back(7);    //Et encore un !
+    
+    //On itère sur la liste
+    for(list<int>::iterator it = liste.begin(); it!=liste.end(); ++it)
+    {
+        cout << *it << endl;
+    }
+    return 0;
+}
